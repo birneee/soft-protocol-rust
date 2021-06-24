2c32b36289f89f75aa875f1103ae2d6eb0bdd637
Benedikt Spies <benedikt.spies@gmail.com>
2021-06-24 23:43:34 +0200

    added function to create new REQ packet

--- soft_shared_lib/src/packet_view/req_packet_view.rs
+++ soft_shared_lib/src/packet_view/req_packet_view.rs
@@ -1,13 +1,35 @@
 use crate::packet::general_soft_packet::GeneralSoftPacket;
 use crate::packet::packet_type::PacketType;
-use crate::soft_error_code::SoftErrorCode;
 use crate::packet_view::unchecked_packet_view::UncheckedPacketView;
+use crate::field_types::{Version, MaxPacketSize, Offset};
+use std::mem::size_of;
+use crate::constants::SOFT_PROTOCOL_VERSION;
 
 pub struct ReqPacketView<'a> {
     inner: UncheckedPacketView<'a>,
 }
 
 impl<'a> ReqPacketView<'a> {
+
+    fn get_required_buffer_size(file_name: &str) -> usize {
+        return size_of::<Version>() +
+            size_of::<PacketType>() +
+            size_of::<MaxPacketSize>() +
+            size_of::<Offset>() +
+            file_name.as_bytes().len()
+    }
+
+    pub fn create_packet_buffer(max_packet_size: MaxPacketSize, file_name: &str) -> Vec<u8> {
+        let mut buf = vec![0u8; Self::get_required_buffer_size(&file_name)];
+        let mut view = UncheckedPacketView::from_buffer(buf.as_mut_slice());
+        view.set_version(SOFT_PROTOCOL_VERSION);
+        view.set_version(SOFT_PROTOCOL_VERSION);
+        view.set_packet_type(PacketType::FileRequestPacket);
+        view.set_max_packet_size(max_packet_size);
+        view.set_file_name(file_name);
+        return buf;
+    }
+
     pub fn from_buffer(buf: &mut [u8]) -> ReqPacketView {
         let inner = UncheckedPacketView::from_buffer(buf);
         assert_eq!(inner.packet_type(), PacketType::FileRequestPacket);
@@ -16,17 +38,25 @@ impl<'a> ReqPacketView<'a> {
         }
     }
 
-    pub fn max_packet_size(&self) -> u16 {
+    pub fn max_packet_size(&self) -> MaxPacketSize {
         self.inner.max_packet_size()
+    }
+
+    pub fn set_max_packet_size(&mut self, val: MaxPacketSize) {
+        self.inner.set_max_packet_size(val);
     }
 
     pub fn file_name(&self) -> String {
         self.inner.file_name()
     }
+
+    pub fn set_file_name(&mut self, val: &str) {
+        self.inner.set_file_name(val);
+    }
 }
 
 impl<'a> GeneralSoftPacket for ReqPacketView<'a> {
-    fn version(&self) -> u8 {
+    fn version(&self) -> Version {
         self.inner.version()
     }
 
