564aa879aafa653125fbb24536c1606f2f235907
Benedikt Spies <benedikt.spies@gmail.com>
2021-06-27 23:20:03 +0200

    completed ERR packet view

--- soft_shared_lib/src/packet_view/err_packet_view.rs
+++ soft_shared_lib/src/packet_view/err_packet_view.rs
@@ -2,12 +2,33 @@ use crate::packet::general_soft_packet::GeneralSoftPacket;
 use crate::packet::packet_type::PacketType;
 use crate::soft_error_code::SoftErrorCode;
 use crate::packet_view::unchecked_packet_view::UncheckedPacketView;
+use std::mem::size_of;
+use crate::field_types::{Version, PacketTypeRaw, ErrorCodeRaw, Padding8, ConnectionId};
+use crate::constants::SOFT_PROTOCOL_VERSION;
 
 pub struct ErrPacketView<'a> {
     inner: UncheckedPacketView<'a>,
 }
 
 impl<'a> ErrPacketView<'a> {
+    fn get_required_buffer_size() -> usize {
+        return size_of::<Version>()
+            + size_of::<PacketTypeRaw>()
+            + size_of::<ErrorCodeRaw>()
+            + size_of::<Padding8>()
+            + size_of::<ConnectionId>();
+    }
+
+    pub fn create_packet_buffer(error_code: SoftErrorCode, connection_id: ConnectionId) -> Vec<u8> {
+        let mut buf = vec![0u8; Self::get_required_buffer_size()];
+        let mut view = UncheckedPacketView::from_buffer(buf.as_mut_slice());
+        view.set_version(SOFT_PROTOCOL_VERSION);
+        view.set_packet_type(PacketType::Err);
+        view.set_error_code(error_code);
+        view.set_connection_id(connection_id);
+        return buf;
+    }
+
     pub fn from_buffer(buf: &mut [u8]) -> ErrPacketView {
         let inner = UncheckedPacketView::from_buffer(buf);
         assert_eq!(inner.packet_type(), PacketType::Err);
@@ -20,13 +41,21 @@ impl<'a> ErrPacketView<'a> {
         self.inner.error_code()
     }
 
-    pub fn connection_id(&self) -> u32 {
+    pub fn set_error_code(&mut self, val: SoftErrorCode) {
+        self.inner.set_error_code(val);
+    }
+
+    pub fn connection_id(&self) -> ConnectionId {
         self.inner.connection_id()
+    }
+
+    pub fn set_connection_id(&mut self, val: ConnectionId) {
+        self.inner.set_connection_id(val);
     }
 }
 
 impl<'a> GeneralSoftPacket for ErrPacketView<'a> {
-    fn version(&self) -> u8 {
+    fn version(&self) -> Version {
         self.inner.version()
     }
 
