806d40e7ce754eb136d197a639233a5c58324ace
Benedikt Spies <benedikt.spies@gmail.com>
2021-06-27 23:15:14 +0200

    renamed config

--- soft_server_lib/src/config.rs
+++ soft_server_lib/src/config.rs
@@ -1,4 +1,4 @@
 pub const FILE_READER_BUFFER_SIZE: usize = 1024;
 
-
-pub const MAX_PACKET_SIZE: usize = 2usize.pow(16) - 8 - 20;
+/// the maximum packet size this SOFT server implementation supports
+pub const SERVER_MAX_PACKET_SIZE: usize = 2usize.pow(16) - 8 - 20;
