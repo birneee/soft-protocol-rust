1a7ce47199563a4409c22d98252726b879f703bd
Vyas G <vyasgiridhar27@gmail.com>
2021-08-07 15:42:59 +0530

    Porting to BufWriter(Seek to Offset), Refractoring.

--- soft_client_lib/src/client.rs
+++ soft_client_lib/src/client.rs
@@ -11,7 +11,7 @@ use soft_shared_lib::packet::packet::Packet::{Acc, Data};
 use soft_shared_lib::packet::packet_buf::PacketBuf;
 use soft_shared_lib::packet::req_packet::ReqPacket;
 use std::fs::{self, File, OpenOptions};
-use std::io::{BufReader, Read, Write};
+use std::io::{BufReader, BufWriter, Read, Seek, SeekFrom, Write};
 use std::net::UdpSocket;
 use std::os::unix::prelude::MetadataExt;
 use std::path::Path;
@@ -19,8 +19,7 @@ use std::sync::atomic::Ordering::SeqCst;
 use std::sync::Arc;
 
 pub const SUPPORTED_PROTOCOL_VERSION: u8 = 1;
-// I had to adjust the MAX PACKET SIZE by a little (-50) to transfer a large file.
-const MAX_PACKET_SIZE: usize = 2usize.pow(16) - 8 - 20 - 50;
+const MAX_PACKET_SIZE: usize = 1188;
 
 pub struct Client {
     state: Arc<ClientState>,
@@ -43,19 +42,21 @@ impl Client {
 
             if let Some(checksum) = checksum {
                 log::debug!("Checksum file found for {}, resuming download.", &filename);
+
                 download_buffer = OpenOptions::new()
                     .read(true)
                     .open(&filename)
                     .expect(format!("File download currupted: {}", &filename).as_str());
                 let metadata = download_buffer.metadata().expect("file error occoured");
-                offset.store(metadata.size(), SeqCst);
-                log::debug!("File Offset for resumption: {}", metadata.size());
+                let current_file_size = metadata.size();
+
+                log::debug!("File Offset for resumption: {}", current_file_size);
+
+                offset.store(current_file_size, SeqCst);
                 state.checksum.store(Some(checksum), SeqCst);
-                // Set the progress to the offset
-                state.progress.store(metadata.size(), SeqCst);
+                state.progress.store(current_file_size, SeqCst);
             } else {
                 log::error!("File already present");
-                // Preemptively exits out of each client operation
                 state.state_type.store(ClientStateType::Downloaded, SeqCst);
             }
         } else {
@@ -131,7 +132,8 @@ impl Client {
 
     /// if the client is already stopped, exits early
     /// Deletes the checksum file from the directory.
-    /// This gets called on any runtime/hard errors.
+    /// This gets called only when the file is invalid
+    /// or the file is Downloaded.
     ///
     fn clean_up(&self) {
         // Don't clean up if there is a error or the client is stopped
@@ -142,6 +144,11 @@ impl Client {
         Client::clean_checksum(&self.filename);
     }
 
+    /// Handshake
+    /// This starts a initial handshake and verifies if the file is changed.
+    /// If the file is different (i.e. Checksums don't match) Handshake again
+    /// With an offset of 0
+    ///
     fn handshake(&self) {
         self.make_handshake();
 
@@ -215,6 +222,7 @@ impl Client {
             .send(send_buf.buf())
             .expect("couldn't send message");
 
+        // TODO: Handle errors.
         self.state
             .socket
             .recv(&mut recv_buf)
@@ -317,10 +325,12 @@ impl Client {
             .store(ClientStateType::Downloading, SeqCst);
         log::info!("Starting download");
 
-        let mut download_buffer = OpenOptions::new()
+        let download_buffer = OpenOptions::new()
             .append(true)
             .open(&self.filename)
-            .expect("Unable to create file for downloading.");
+            .expect("Unable to open file for downloading.");
+        let mut writer = BufWriter::new(download_buffer);
+        writer.seek(SeekFrom::Start(self.offset.load(SeqCst))).expect("Unable to seek to offset");
 
         let mut recv_buf = [0; MAX_PACKET_SIZE];
         let mut progress = self.state.progress.load(SeqCst);
@@ -338,7 +348,7 @@ impl Client {
                     eprintln!("received unsupported packet");
                 }
                 Ok(Data(p)) => {
-                    let _ = download_buffer.write(p.data());
+                    let _ = writer.write(p.data());
                     let send_buf = PacketBuf::Ack(AckPacket::new_buf(
                         1,
                         connection_id,
