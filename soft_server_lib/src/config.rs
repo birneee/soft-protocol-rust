acedce8b9b7dc8795e418673044bf8e3d63ad21b
Benedikt Spies <benedikt.spies@gmail.com>
2021-08-09 01:15:34 +0200

    remove old server implementation

--- soft_server_lib/src/config.rs
+++
@@ -1,9 +0,0 @@
-pub const FILE_READER_BUFFER_SIZE: usize = 2usize.pow(16);
-
-/// the maximum packet size this SOFT server implementation supports
-pub const SERVER_MAX_PACKET_SIZE: usize = 2usize.pow(16) - 8 - 20;
-
-/// maximum simultaneous connections supported by the server
-///
-/// if this is exceeded the server might drop connections
-pub const MAX_SIMULTANEOUS_CONNECTIONS: usize = 100;
